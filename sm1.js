bb3bf4ca5bb3baf68c46008b486e3ac5a4aabeac
ddddive <651137620@qq.com>
2021-09-08 17:11:24 +0800

    first commit

---
+++ sm1.js
@@ -0,0 +1,70 @@
+//canvas
+var c=document.getElementById("myCanvas");
+var cxt=c.getContext("2d");
+cxt.fillStyle="lightgreen";
+cxt.fillRect(0,0,700,500);
+
+var sdot=c.getContext("2d");
+sdot.fillStyle="black";
+shead = sdot.fillRect(340,240,10,10);
+sfoot = sdot.fillRect(350,240,10,10);
+
+var fdot = c.getContext("2d");
+fdot.fillStyle = "red";
+fdot.fillRect(50,50,10,10);
+
+var overdot = c.getContext("2d");
+
+var w;
+
+function startWorker() {
+    if(typeof(Worker) !== "undefined") {
+        if(typeof(w) == "undefined") {
+            w = new Worker("sm1worker.js");
+            startmove();
+        }
+/*
+        w.onmessage = function(event) {
+            document.getElementById("result").innerHTML = event.data;
+        };
+*/ 
+    } else {
+        document.getElementById("result").innerHTML = "Sorry! No Web Worker support.";
+    }
+
+    document.getElementById("sbd").onkeyup = function(e) {
+        // 按键 keyCode弃用，改用key
+        switch(e.key) {
+            case 'ArrowLeft':
+                keyCodes = 'ArrowLeft';
+                break;
+            case 'ArrowUp':
+                keyCodes = 'ArrowUp';
+                break;
+            case 'ArrowRight':
+                keyCodes = 'ArrowRight';
+                break;
+            case 'ArrowDown':
+                keyCodes = 'ArrowDown';
+                break;
+        }
+        //console.log("keyuop   "+keyCodes+""+e.key);
+    };
+}
+
+function stopWorker() { 
+    w.terminate();
+    w = undefined;
+    clearTimeout(myVar);
+    //console.log("stop");
+}
+
+function cnvs_getCoordinates(e) {
+    x=e.clientX;
+    y=e.clientY;
+    document.getElementById("xycoordinates").innerHTML="Coordinates: (" + x + "," + y + ")";
+}
+ 
+function cnvs_clearCoordinates() {
+    document.getElementById("xycoordinates").innerHTML="";
+}
---
+++ sm1worker.js
@@ -0,0 +1,108 @@
+
+
+//snake dot
+let snakedot = [[340,240],[350,240]];
+let whilehead = [[340,240]];
+// let bodydot = [];
+let fooddot = [[50,50]];
+let keyCodes = 'ArrowLeft';
+// game over dot
+/**
+ * let overdots = [
+    [180,80],[170,80],[160,80],[150,80],[140,80],
+    [140,90],[140,100],[140,110],[140,120],[140,130],
+    [180,130],[170,130],[160,130],[150,130],[140,130],
+    [180,120],[180,110],[180,100],[170,100],[160,100]];
+ */
+
+function startmove(){
+
+
+    if(snakedot[0][0]>0 && snakedot[0][0]<690 && snakedot[0][1]>0 && snakedot[0][1]<490) {
+
+        //把头坐标放在暂存头
+        switch(keyCodes) {
+            case 'ArrowUp' ://up
+                whilehead.unshift([snakedot[0][0],snakedot[0][1]-10]);
+                break;
+            case 'ArrowRight' ://right
+                whilehead.unshift([snakedot[0][0]+10,snakedot[0][1]]);
+                break;
+            case 'ArrowDown' ://down
+                whilehead.unshift([snakedot[0][0],snakedot[0][1]+10]);
+                break;
+            case 'ArrowLeft'://left
+                whilehead.unshift([snakedot[0][0]-10,snakedot[0][1]]);
+                break;
+            default:
+                console.log("nononono");
+                break;
+        }
+        // 修改snakedot数组
+        // 新头点插入 数组头，删除尾部
+        if(fooddot.length !=0) { 
+            eat();
+        }else {
+            snakedot.unshift(whilehead[0]);
+            delfoot();
+        }
+        // console.log(snakedot);
+
+        //新增头在画布上
+        sdot.fillStyle="black";
+        sdot.fillRect(snakedot[0][0],snakedot[0][1],10,10);
+
+        myVar = setTimeout("startmove()",70);
+    } else {
+        console.log("game over");
+        overdot.fillStyle = "DodgerBlue";
+        overdot.font = "bold 100px Arial";
+        overdot.fillText("Game Over",100,160);
+/*
+        for (let i in overdots) {
+            overdot.fillRect(overdots[i][0],overdots[i][1],10,10);
+        }
+*/
+
+    }
+}
+// 判断吃食物
+function eat() { // 函数可能可以 if（whilehead in fooddot）  这个in可以构造一个函数代替
+    for (let i in fooddot) {
+
+        if (whilehead[0][0]==fooddot[i][0] && whilehead[0][1]==fooddot[i][1]) {
+            
+            snakedot.unshift(fooddot[i])
+
+            // 食物吃掉就在食物数组中去掉
+            fooddot.splice(i, 1);
+            newfood();
+        }else {
+            snakedot.unshift(whilehead[0]);
+            // console.log(snakedot[0]+" + " +whilehead[0] )
+            delfoot();
+        }
+    }
+}
+// 删除尾点
+function delfoot() {
+    //删除尾部像素
+    sdot.clearRect(snakedot[snakedot.length-1][0],snakedot[snakedot.length-1][1],10,10);
+    //补充删除后的空白
+    sdot.fillStyle="lightgreen";
+    sdot.fillRect(snakedot[snakedot.length-1][0],snakedot[snakedot.length-1][1],10,10);
+    
+    snakedot.pop();
+}
+
+// 随机生成food
+function newfood() {
+    if (fooddot.length == 0) {
+        
+        fooddot.unshift([(Math.floor(Math.random()*69)+1)*10 , (Math.floor(Math.random() * 49) + 1)*10])
+        fdot.fillStyle = "red";
+        fdot.fillRect(fooddot[0][0],fooddot[0][1],10,10);
+
+    }
+}
+
